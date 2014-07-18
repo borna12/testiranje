372fbfb9a735f2e8edd1a55e7a1103fda6b0a5d5
borna <jmihalje2@ffzg.hr>
2014-07-18 20:51:02 +0200

    osmi i deveti
    kolizije

--- Assets/code/igrac.cs
+++ Assets/code/igrac.cs
@@ -3,13 +3,47 @@ using System.Collections;
 
 public class igrac : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
 	
+	private bool _isFacingRight;
+	private kontrolerzalika _controller;
+	private float _normalizedHorizontalSpeed;
+
+	public float MaxSpeed=8;
+	public float SpeedAccelerationOnGround =10f;
+	public float SpeedAccelerationInAir=5f;
+
+	public void Start(){
+		_controller = GetComponent<kontrolerzalika> ();
+		_isFacingRight = transform.localScale.x > 0;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void Update(){
+		HandleInput ();
+
+		var movementFactor = _controller.State.IsGrounded ? SpeedAccelerationOnGround : SpeedAccelerationInAir;
+		_controller.SetHorizontalForce(Mathf.Lerp(_controller.Velocity.x,_normalizedHorizontalSpeed*MaxSpeed, Time.deltaTime*movementFactor));
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetKey (KeyCode.D)) {
+						_normalizedHorizontalSpeed = 1;
+						if (!_isFacingRight)
+								Flip ();} 
+		else if (Input.GetKey (KeyCode.A)) {
+						_normalizedHorizontalSpeed = -1;
+						if (_isFacingRight)
+								Flip ();} 
+		else {
+			_normalizedHorizontalSpeed=0;	
+		}
+
+		if (_controller.CanJump && Input.GetKeyDown (KeyCode.Space)) {
+			_controller.Jump();		
+		}
+	}
+
+	private void Flip(){
+		transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		_isFacingRight = transform.localScale.x > 0;
 	}
 }
--- Assets/code/kontrolerzalika.cs
+++ Assets/code/kontrolerzalika.cs
@@ -13,45 +13,167 @@ public class kontrolerzalika : MonoBehaviour {
 	public kontrolerparametar DefaultParameters;
 
 	public kontrolerstanja2d State { get; private set; }
+	public Vector2 Velocity { get {return _velocity;}}
+	public bool CanJump{get{return false;}}
+	public bool HandleCollisions { get; set;}
+	public kontrolerparametar Parameters{get{return _overrideParameters ?? DefaultParameters;}}
+
+	private Vector2 _velocity;
+	private Transform _transform;
+	private Vector3 _localScale;
+	private BoxCollider2D _boxCollider;
+	private kontrolerparametar _overrideParameters;
+	private Vector3
+		_raycastTopLeft,
+		_raycastBottomRight,
+		_raycastBottomLeft;
+
+	private float
+				_verticalDistanceBetweenRays,
+				_horizontalDistanceBetweenRays;
 
 	public void Awake()
-	{}
-	public void AddForce(Vector2 force)
-	{}
+	{
+		HandleCollisions = true;
+		State=new kontrolerstanja2d();
+		_transform = transform;
+		_localScale = transform.localScale;
+		_boxCollider = GetComponent<BoxCollider2D>();
 
-	public void SetForce(Vector2 force){}
+		var coliderWidth = _boxCollider.size.x * Mathf.Abs (transform.localScale.x) - (2 * SkinWidth);
+		_horizontalDistanceBetweenRays = coliderWidth / (TotalVerticalRays - 1);
+
+		var colliderHeight = _boxCollider.size.y * Mathf.Abs (transform.localScale.y)-(2*SkinWidth);
+		_verticalDistanceBetweenRays = colliderHeight / (TotalHorizontalRays - 1);
+	}
+	public void AddForce(Vector2 force)
+	{
+		_velocity = force;
+	}
+
+	public void SetForce(Vector2 force){
+		_velocity += force;
+	}
 
 	public void SetHorizontalForce(float x)
-	{}
+	{
+		_velocity.x = x;
+	}
 
 	public void SetVerticalForce(float y)
-	{}
+	{
+		_velocity.y = y;
+	}
 
 	public void Jump()
-	{}
+	{
 
+	}
 	public void LateUpdate()
-	{}
+	{
+		Move (Velocity * Time.deltaTime);
+	}
 	public void Move(Vector2 deltaMovement)
-	{}
+	{
+		var wasGrounded = State.IsCollidingBelow;
+		State.Reset ();
+
+		if (HandleCollisions) {
+			HandlePlatforms();
+			CalcualteRayOrigins();
+
+			if (deltaMovement.y<0 && wasGrounded){
+				HandleVerticalSlope(ref deltaMovement);
+			}
+			if (Mathf.Abs(deltaMovement.x)>.001f)
+				MoveHorizontally(ref deltaMovement);
+
+			MoveVertically(ref deltaMovement);
+		}
+		_transform.Translate (deltaMovement,Space.World);
+
+		//kasnije dorađuje se za kretnju
+		if (Time.deltaTime > 0)
+						_velocity = deltaMovement / Time.deltaTime;
+
+		_velocity.x = Mathf.Min (_velocity.x, Parameters.MaxVelocity.x);
+		_velocity.y = Mathf.Min (_velocity.y, Parameters.MaxVelocity.y);
+
+		if (State.IsMovingDownSlope)
+						_velocity.y = 0;
+	}
 	private void HandlePlatforms()
-	{}
-	private void RaycastOrigons()
-	{}
+	{
+
+	}
+	private void RayCastOrigins()
+	{
+
+	}
 	private void CalcualteRayOrigins()
-	{}
+	{
+		var size = new Vector2 (_boxCollider.size.x * Mathf.Abs (_localScale.x), _boxCollider.size.y * Mathf.Abs (_localScale.y)) / 2;
+		var center = new Vector2 (_boxCollider.center.x * _localScale.x, _boxCollider.center.y * _localScale.y);
+
+		_raycastTopLeft=_transform.position+new Vector3(center.x-size.x + SkinWidth, center.y + size.y -SkinWidth);
+		_raycastBottomRight = _transform.position + new Vector3 (center.x + size.x - SkinWidth, center.y - size.y + SkinWidth);
+		_raycastBottomLeft = _transform.position+new Vector3(center.x-size.x+SkinWidth, center.y-size.y+SkinWidth);
+
+	}
 	private void MoveHorizontally(ref Vector2 deltaMovement)
-	{}
+	{
+		var isGoingRight = deltaMovement.x > 0;
+		var rayDistance = Mathf.Abs (deltaMovement.x) * SkinWidth;
+		var rayDirection = isGoingRight ? Vector2.right : -Vector2.right;
+		var rayOrigin = isGoingRight ? _raycastBottomRight : _raycastBottomLeft;
+
+		for (var i=0; i<TotalHorizontalRays; i++) {
+				
+			var rayVector = new Vector2(rayOrigin.x, rayOrigin.y + (i*_verticalDistanceBetweenRays));
+			Debug.DrawRay(rayVector,rayDirection*rayDistance,Color.red);
+
+			var rayCastHit=Physics2D.Raycast(rayVector, rayDirection, rayDistance, PlatformMask);
+
+			if(!rayCastHit)
+				continue;
+			if (i==0 && HandleHorizontalSlope(ref deltaMovement, Vector2.Angle(rayCastHit.normal,Vector2.up), isGoingRight))
+				break;
+			deltaMovement.x=rayCastHit.point.x-rayVector.x;
+			rayDistance=Mathf.Abs(deltaMovement.x);
+
+			if (isGoingRight){
+				deltaMovement.x-=SkinWidth;
+				State.IsCollidingRight=true;
+			}
+			else{
+				deltaMovement.x+=SkinWidth;
+				State.IsCollidingLeft=true;
+			}
+
+			if (rayDistance<SkinWidth + .0001f)
+				break;
+		}
+	}
 	private void MoveVertically(ref Vector2 deltaMovement)
-	{}
+	{
+
+	}
 	private void HandleVerticalSlope(ref Vector2 deltaMovement)
-	{}
-	private void HandleHorizontalSlope(ref Vector2 deltaMovement, float angle, bool isGoingRight)
-	{}
+	{
+
+	}
+	private bool HandleHorizontalSlope(ref Vector2 deltaMovement, float angle, bool isGoingRight)
+	{
+		return false;
+	}
 	public void OnTriggerEnter2D(Collider2D other)
-	{}
+	{
+
+	}
 	public void OnTriggerExit2D(Collider2D other)
-	{}
+	{
+
+	}
 
 }
 	                 
