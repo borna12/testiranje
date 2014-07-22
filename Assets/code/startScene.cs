187eff9b067272c1b37f80a3af8b30061afb0a8c
borna <jmihalje2@ffzg.hr>
2014-07-23 01:42:11 +0200

    33

---
+++ Assets/code/startScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startScene : MonoBehaviour
+{
+
+    public string FirstLevel;
+
+    public void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        gamemanager.Instance.Reset();
+        Application.LoadLevel(FirstLevel);
+
+
+    }
+}
