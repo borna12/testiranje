928be48831e40aa35efd5e640f1550031927117c
borna <jmihalje2@ffzg.hr>
2014-07-22 20:40:15 +0200

    31

--- Assets/code/simpleEnemyAi.cs
+++ Assets/code/simpleEnemyAi.cs
@@ -1,69 +1,73 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Configuration;
+using UnityEngine;
 
 public class simpleEnemyAi : MonoBehaviour, ITakeDamage,IPlayerRespawnListener 
 {
-	public float Speed;
-	public float FireRate=1;
-	public projektil Projectile;
+    public float Speed;
+    public float FireRate = 1;
+    public projektil Projectile;
     public GameObject DestroyedEffect;
-    public int PointsToGiveToPlayer;
+    public int PointsToGivePlayer;
 
-	private kontrolerzalika _controller;
-	private Vector2 _direction;
-	private Vector2 _startPosition;
-	private float _canFireIn;
+    private kontrolerzalika _controller;
+    private Vector2 _direction;
+    private Vector2 _startPosition;
+    private float _canFireIn;
 
-	public void Start()
-	{
-		_controller = GetComponent<kontrolerzalika> ();
-		_direction = new Vector2 (-1, 0);
-		_startPosition = transform.position;
-	}
-	public void Update()
-	{
-		_controller.SetHorizontalForce (_direction.x * Speed);
+    public void Start()
+    {
+        _controller = GetComponent<kontrolerzalika>();
+        _direction = new Vector2(1, 0);
+        _startPosition = transform.position;
 
-		if ((_direction.x < 0 && _controller.State.IsCollidingLeft) || (_direction.x > 0 && _controller.State.IsCollidingRight)) {
+    }
 
+    public void Update()
+    {
+        _controller.SetHorizontalForce(_direction.x * Speed);
+
+        if ((_direction.x < 0 && _controller.State.IsCollidingLeft) ||
+            (_direction.x > 0 && _controller.State.IsCollidingRight))
+        {
             _direction = -_direction;
-			transform.localScale=new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-		}
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
 
-		if ((_canFireIn-=Time.deltaTime)>0)
-			return;
+        if ((_canFireIn -= Time.deltaTime) > 0)
+            return;
 
-		var raycast=Physics2D.Raycast(transform.position, _direction, 10, 1<<LayerMask.NameToLayer("igrac"));
-			if (!raycast)
-				return;
-		var projectile = (projektil) Instantiate(Projectile, transform.position, transform.rotation);
-		projectile.Initialize(gameObject,_direction, _controller.Velocity);
-		_canFireIn=FireRate;
-	}
+        var raycast = Physics2D.Raycast(transform.position, _direction, 10, 1 << LayerMask.NameToLayer("igrac"));
+        if (!raycast)
+            return;
 
-	public void TakeDamage(int damage, GameObject instigator)
-	{
-	    if (PointsToGiveToPlayer != 0)
-	    {
-	        var projectile = instigator.GetComponent<projektil>();
-	        if (projectile != null && projectile.Owner.GetComponent<igrac>() != null)
-	        {
-                gamemanager.Instance.AddPoints(PointsToGiveToPlayer);
-                FloatingText.Show(string.Format("+{0}!", PointsToGiveToPlayer),"PointStarText", new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
-	        }
-	    }
-	    
+        var projectile = (projektil)Instantiate(Projectile, transform.position, transform.rotation);
+        projectile.Initialize(gameObject, _direction, _controller.Velocity);
+        _canFireIn = FireRate;
 
-	    Instantiate(DestroyedEffect,transform.position,transform.rotation);
-		gameObject.SetActive (false);
-	}
+    }
 
-	public void OnPlayerRespawnInThicCheckpoint(checkpoint check, igrac player)
-	{
-		_direction = new Vector2 (-1, 0);
-		transform.localScale = new Vector3 (1, 1, 1);
-		transform.position=_startPosition;
-		gameObject.SetActive (true);
+    public void TakeDamage(int damage, GameObject instigator)
+    {
+        if (PointsToGivePlayer != 0)
+        {
+            var projectile = instigator.GetComponent<projektil>();
+            if (projectile != null && projectile.Owner.GetComponent<igrac>() != null)
+            {
+                gamemanager.Instance.AddPoints(PointsToGivePlayer);
+                FloatingText.Show(string.Format("+{0}!", PointsToGivePlayer), "PointStarText",
+                    new FromWorldPointTextPositioner(Camera.main, transform.position, 1.5f, 50));
+            }
+        }
 
-	}
+        Instantiate(DestroyedEffect, transform.position, transform.rotation);
+        gameObject.SetActive(false);
+    }
+
+    public void OnPlayerRespawnInThicCheckpoint(checkpoint checkpoint, igrac player)
+    {
+        _direction = new Vector2(-1, 0);
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.position = _startPosition;
+        gameObject.SetActive(true);
+    }
 }
