446a081128b19799cf0a6102b96f76162d2da5b5
borna <jmihalje2@ffzg.hr>
2014-07-23 18:11:14 +0200

    zajebancija

---
+++ Assets/code/EndGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndGame : MonoBehaviour
+{
+    public void Update()
+    {
+
+        if (!Input.GetMouseButtonDown(0))
+            Application.Quit();
+
+    }
+}
